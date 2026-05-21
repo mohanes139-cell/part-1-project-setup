a1d6937c77689a261d67c2232b51be5e706a9666
Mohan <mohan.e.s.139@kalvium.community>
2026-05-21 10:08:10 +0530

    feat: connect RestCountries API with live search and country cards

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,5 +1,29 @@
-function CountryCard() {
-  return null
+import { Link } from 'react-router-dom'
+
+function CountryCard({ country }) {
+  const { name, flags, population, region, capital, cca3 } = country
+
+  return (
+    <Link to={`/country/${cca3}`} className="card">
+      <img
+        className="card__flag"
+        src={flags.svg}
+        alt={`${name.common} flag`}
+      />
+      <div className="card__body">
+        <h3 className="card__name">{name.common}</h3>
+        <p>
+          <span>Population:</span> {population.toLocaleString()}
+        </p>
+        <p>
+          <span>Region:</span> {region}
+        </p>
+        <p>
+          <span>Capital:</span> {capital?.[0] ?? 'N/A'}
+        </p>
+      </div>
+    </Link>
+  )
 }
 
 export default CountryCard
